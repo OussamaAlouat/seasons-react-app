38fb0d7a27ac95f6a75ba4b81056debd2f5f62f2
OussamaAlouat <al317257@uji.es>
2020-08-19 13:22:27 +0200

    Set a default props

--- src/Spinner.js
+++ src/Spinner.js
@@ -4,9 +4,13 @@ const Spinner = (props) => {
   const { message } = props;
   return (
     <div className="ui active inverted dimmer">
-      <div className="ui big text loader">{message}</div>
+      <div className="ui big text loader">{ message }</div>
     </div>
   );
 }
+
+Spinner.defaultProps = {
+  message: 'Loading...',
+};
 
 export default Spinner;
