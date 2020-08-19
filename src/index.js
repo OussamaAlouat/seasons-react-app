b632ed1f0b621f1a596eb8e05a11ad82c1af3f1b
OussamaAlouat <al317257@uji.es>
2020-08-19 13:25:26 +0200

    Lint code

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
+      return <SeasonDisplay lat={ this.state.lat }/>
     }
 
     return <Spinner message='Please accept location reqyuest'/>
