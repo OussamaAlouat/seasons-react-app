3507cb736bf0c5621a96d2dbe5dfe27d3732d7ea
OussamaAlouat <al317257@uji.es>
2020-08-19 13:20:05 +0200

    FIxed errors

--- src/Spinner.js
+++ src/Spinner.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Spinner = (props) => {
   const { message } = props;
   return (
-    <div class="ui active inverted dimmer">
-      <div class="ui big text loader">{message}</div>
+    <div className="ui active inverted dimmer">
+      <div className="ui big text loader">{message}</div>
     </div>
   );
 }
