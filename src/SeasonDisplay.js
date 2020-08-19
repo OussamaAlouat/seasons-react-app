56931046c1afa6c87fa2be82a3ba933b92de0dc9
Oussama Alouat <oussama.alouat@amaris.com>
2020-08-19 10:50:38 +0200

    Determine my current season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  }
+
+  return lat > 0 ? 'winter' : 'summer';
+}
+
 const SeasonDisplay = (props) => {
   const { lat } = props;
-  return <div>Latitude: { lat }</div>
+  const season = getSeason(lat, new Date().getMonth());
+return <div> The season is: { season } </div>
 };
 
 export default SeasonDisplay;
